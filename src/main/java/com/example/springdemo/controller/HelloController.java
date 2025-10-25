5e45712a52e13e866a70b271cd47071c22894b6b
hamidsoleymani <hamid.soleymani@gmx.de>
2025-10-25 15:05:42 +0200

    controller zeit

--- src/main/java/com/example/springdemo/controller/HelloController.java
+++ src/main/java/com/example/springdemo/controller/HelloController.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 public class HelloController {
 
 
-    @GetMapping("/time")
+    @GetMapping("/zeit")
     public String time() {
        return LocalDateTime.now().toString();
     }
